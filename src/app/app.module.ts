9aa6956d2770caff8777357116ff9887ff3339ab
Vladimir Denisov <vdenisov.dev@gmail.com>
2018-06-05 21:14:58 +0300

    add home and todos pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { SharedModule } from './shared/shared.module';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './todos/todos.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'todos', component: TodosComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
+    
     SharedModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
